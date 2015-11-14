13d4ff020c63ef15da0ce88ea93a2f4ebe0771b7
unknown <mab7799@NM2550-21.main.ad.rit.edu>
2015-11-14 18:16:49 -0500

    Added creepy sewer pipe to camp zone. Miscellaneous tree and texture work Added script to play sounds once on trigger.

---
+++ Assets/PlaySoundOnHit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnHit : MonoBehaviour {
+	
+	bool fired;
+
+	// Use this for initialization
+	void Start () {
+		fired = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (fired == false) {
+			GetComponent<AudioSource>().Play();
+			fired = true;
+		}
+	}
+}
